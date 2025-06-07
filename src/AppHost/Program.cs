294ed442cba6339a0ff364a952378143434401ad
Sunkanmi Olawuwo <sunkanmiolawuwo.gas@gmail.com>
2025-06-07 22:07:53 +0100

    Fix issues with the database volume failing

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -1,26 +1,34 @@
+using AppHost;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 /*Backing Services */
 
 var postgres = builder
-    .AddPostgres("postgres")
+    .AddPostgres(AppHostConstants.PostgresServerBackingService)
     .WithPgAdmin()
-    .WithDataVolume()
     .WithLifetime(ContainerLifetime.Persistent);
 
-var pfmDb = postgres.AddDatabase("pfmdb");
+var pfmDb = postgres.AddDatabase(AppHostConstants.PfmDbBackingService);
+
+if (builder.ExecutionContext.IsRunMode)
+{
+    postgres.WithDataVolume(AppHostConstants.PfmDbVolumeBackingService);
+}
 
 /*Backing Services*/
 
 /*Projects*/
 
 var migrationService = builder
-    .AddProject<Projects.PersonalFinanceManager_MigrationService>("migrationservice")
+    .AddProject<Projects.PersonalFinanceManager_MigrationService>(
+        AppHostConstants.MigrationServiceProject
+    )
     .WithReference(pfmDb)
     .WaitFor(pfmDb);
 
 builder
-    .AddProject<Projects.PersonalFinanceManager_API>("apiservice")
+    .AddProject<Projects.PersonalFinanceManager_API>(AppHostConstants.ApiServiceProject)
     .WithReference(pfmDb)
     .WaitFor(pfmDb)
     .WaitFor(migrationService);
